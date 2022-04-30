5632c109a1941056616f49cab19ea81c9d07e518
hanseonghui <pinedelta@icloud.com>
2022-04-30 23:53:27 +0900

    SignupForm

---
+++ somang/src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignupForm from "../components/SignupForm/SignupForm";
+
+function Signup() {
+  return (
+    <>
+      <SignupForm />
+    </>
+  );
+}
+
+export default Signup;
