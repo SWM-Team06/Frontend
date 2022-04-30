7037503e41c7b5d1b289eb3b674042d1e51d1a98
hanseonghui <pinedelta@icloud.com>
2022-04-30 23:53:44 +0900

    Add Router

--- somang/src/Router.js
+++ somang/src/Router.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
