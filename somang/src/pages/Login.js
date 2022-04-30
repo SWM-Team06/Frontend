f4dc44f67f8f78fda5a809cd69eca05464e1de79
hanseonghui <pinedelta@icloud.com>
2022-04-30 23:53:18 +0900

    LoginForm

---
+++ somang/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/LoginForm/LoginForm";
+
+function Login() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+}
+
+export default Login;
